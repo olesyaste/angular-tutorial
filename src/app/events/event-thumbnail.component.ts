ce640dcb282bdec125c19bf8b433c5b3e60c9422
Olesya Steinmetz <newsabome@icloud.com>
2021-02-12 17:43:21 +0100

    some new features

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -3,10 +3,14 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
 @Component({
     selector: 'event-thumbnail',
     template: `
-        <div class="well hoverwell thumnail"> 
+        <div [routerLink]="['/events', event.id]" class="well hoverwell thumbnail"> 
             <h2>{{event.name}}</h2>
             <div>Date: {{event.date}}</div>
-            <div>Time: {{event.time}}</div>
+            <div [ngClass]="getStartTimeClass()"[ngSwitch]="event?.time"> Time: {{event.time}} 
+                <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+                <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+                <span *ngSwitchDefault>(Normal Start) </span>
+            </div>
             <div>Price: \${{event.price}}</div>
             <div>
                 <span>Location: {{event.location.address}}</span>
@@ -15,6 +19,9 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
         </div>
     `,
     styles: [`
+        .bold { font-weight: bold; }
+        .green { color:  #003300 !important; }
+        .thumbnail { min-height: 210px; }
         .pad-left { margin-left: 10px; }
         .well div { color: #bbb; }
     `]
@@ -22,5 +29,8 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
 
 export class EventThumbnailComponent {
    @Input()event:any
-   
+   getStartTimeClass(){
+     if(this.event && this.event.time === '8:00 am') return 'green bold'
+     return ''
+   }
 }
